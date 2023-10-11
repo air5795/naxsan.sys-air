e7f62066c61187077111bd98d655c27b9d28e386
Ale Ofi <air5795@gmail.com>
2023-10-11 12:31:07 -0400

    cambios en index

--- index.php
+++ index.php
@@ -63,12 +63,12 @@ if (!empty($_SESSION['active'])) {
   <link rel="stylesheet" href="css/style.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link rel="shortcut icon" href="img/icono2.png">
+  <link rel="shortcut icon" href="img/ICON.png">
   <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@300;600&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 </head>
 
-<body class="" style="background-color: lightgrey;color: black;">
+<body class="" style="background-color: black ;color: white;">
 
   <section class="container-fluid">
     <div class="row g-0">
@@ -111,7 +111,7 @@ if (!empty($_SESSION['active'])) {
       <div class="col-lg-4  ">
         <div class="px-lg-6 pt-lg-4 pb-lg-3 p-4 w-100 mb-auto">
 
-          <center><img src="img/ICONO2.PNG" class=""></center>
+          <center><img src="img/ICON.PNG" width="80%" class="" ></center>
         </div>
         <div class="px-lg-5 py-lg-4 p-4 w-100 align-self-center">
 
@@ -120,6 +120,11 @@ if (!empty($_SESSION['active'])) {
           <!-- Formulario de envio de datos login -->
 
           <form action="" method="post">
+
+          <h2 style="font-size:18px;text-align: center;">SISTEMA DE GESTION, CONTROL E INFORMACION </h2> 
+          <h2 style="font-size:15px;text-align: center;">CONSTRUCTORA COMERCIALIZADORA NAXSAN </h2>
+
+          <hr>
 
             <div class="mb-4">
               <label for="disabledTextInput" class="form-label font-weight-bold"><i class="bi bi-person"></i> Usuario</label>
@@ -142,7 +147,9 @@ if (!empty($_SESSION['active'])) {
         </div>
 
         <div class="text-center px-lg-5 pt-lg-3 pb-lg-4 p-4 w-100 mt-auto">
-          <p class="d-inline-block mb-0 ">No tienes una cuenta? </p> <a href="#" class=" text-decoration-none">Registrate aqui </a>
+          <p class="d-inline-block mb-0 ">@irsoft-  </p> <a href="#" class=" text-decoration-none"> SYSTEMS </a> <BR>
+          <HR></HR>
+          <p class="d-inline-block mb-0 ">LICENCIA :  </p> <a style="color: greenyellow !IMPORTANT;" href="#" class=" text-decoration-none"> ACTIVA </a>
         </div>
       </div>
 
