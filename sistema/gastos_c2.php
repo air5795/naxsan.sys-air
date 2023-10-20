0906fb48700a334564fe9703aa1dc879c9026349
Ale Ofi <air5795@gmail.com>
2023-10-20 09:44:46 -0400

    cambiso cja la paz

--- sistema/gastos_c2.php
+++ sistema/gastos_c2.php
@@ -316,19 +316,17 @@
                                     //$query = mysqli_query($conexion, "SELECT * FROM gastos
                                     //ORDER BY id_gasto DESC;");
 
-                                    $query = mysqli_query($conexion, "SELECT
-ROW_NUMBER() 
-OVER(ORDER BY id_gastoC ) 
-row_num,
-id_gastoC,
-g_proyecto, 
-g_detalleGasto,
-g_montoBs,
-g_montoU,
-g_fechai,
-g_respaldo
-FROM gastos_c2
-ORDER BY id_gastoC DESC;");
+                                    $query = mysqli_query($conexion, "SELECT 
+                                    (@row_number:=@row_number + 1) AS row_num,
+                                    id_gastoC,
+                                    g_proyecto, 
+                                    g_detalleGasto,
+                                    g_montoBs,
+                                    g_montoU,
+                                    g_fechai,
+                                    g_respaldo
+                                FROM gastos_c2
+                                ORDER BY id_gastoC DESC");
 
                                 
 
